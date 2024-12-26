ffc93e82e1557ac0e0801a05d456f4152620c170
srs-shihab <sabbir.reza@bevycommerce.com>
2024-12-26 22:33:18 +0600

    create custom product grid

--- components/home/HomeFirstProductGrid.tsx
+++ components/home/HomeFirstProductGrid.tsx
@@ -3,46 +3,105 @@ import ProductsGrid from "../common/ProductsGrid";
 import RenderImageAndProducts from "../common/RenderImageAndProducts";
 
 export default function HomeFirstProductGrid() {
+  const renderFunction = (url: any) => {
+    return (
+      <RenderImageAndProducts
+        renderType="image"
+        imageFileOrUrl={`${url}`}
+        hoverProducts={[
+          {
+            position: { x: 10, y: 15 },
+            imgUrl:
+              "https://gulfpalms.com/wp-content/uploads/2023/08/Cholorophytum-2-300x300.jpg",
+            productId: "1",
+            price: "25.99",
+            description: "Product 1",
+          },
+          {
+            position: { x: 50, y: 50 },
+            imgUrl:
+              "https://gulfpalms.com/wp-content/uploads/2023/08/Cholorophytum-2-300x300.jpg",
+            productId: "2",
+            price: "39.99",
+            description: "Product 2",
+          },
+        ]}
+      />
+    );
+  };
+
   const items = [
     {
-      id: 3,
+      id: 5,
       content: (
-        <RenderImageAndProducts
-          renderType="image"
-          imageFileOrUrl="https://gulfpalms.com/wp-content/uploads/2023/10/3123600by600-thumb-430x430.jpg"
-          hoverProducts={[
-            {
-              position: { x: 10, y: 15 },
-              imgUrl:
-                "https://gulfpalms.com/wp-content/uploads/2023/08/Cholorophytum-2-300x300.jpg",
-              productId: "1",
-              price: "25.99",
-              description: "Product 1",
-            },
-            {
-              position: { x: 50, y: 50 },
-              imgUrl:
-                "https://gulfpalms.com/wp-content/uploads/2023/08/Cholorophytum-2-300x300.jpg",
-              productId: "2",
-              price: "39.99",
-              description: "Product 2",
-            },
-          ]}
-        />
+        <div
+          style={{
+            width: "100%",
+            height: "100%",
+            backgroundImage: `url(https://gulfpalms.com/wp-content/uploads/2023/10/DSC08580.jpg)`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+          className="relative"
+        >
+          <div className="absolute top-1 left-0 h-full w-full bg-black bg-opacity-30 p-5 text-white space-y-[10px]">
+            <p className="text-xl">PLANTS DOCTORS</p>
+            <p className="text-2xl font-bold">GULF PALM</p>
+            <p className="">
+              Gulf Palms caters to a wide scope of clients ranging from
+              Governmental, Commercial, Industrial to Residential. Our
+              experienced team provides customized solutions from design to
+              execution based on our clients.
+            </p>
+          </div>
+        </div>
       ),
-      columnSpan: 1,
-      rowSpan: 22,
+      col: 1,
+      rowSpan: "20%",
+      height: "110%",
     },
-    { id: 1, content: "Product 1", columnSpan: 2, rowSpan: 1 },
-    { id: 2, content: "Product 2", columnSpan: 1, rowSpan: 1 },
-    { id: 4, content: "Product 4", columnSpan: 1, rowSpan: 1 },
-    { id: 5, content: "Product 5", columnSpan: 2, rowSpan: 1 },
+    {
+      id: 1,
+      content: renderFunction(
+        "https://gulfpalms.com/wp-content/uploads/2023/10/3123600by600-thumb-430x430.jpg"
+      ),
+      col: 2,
+      rowSpan: "20%",
+      height: "60%",
+    },
+    {
+      id: 3,
+      content: renderFunction(
+        "https://gulfpalms.com/wp-content/uploads/2023/10/lkl600by800-thumb.jpg"
+      ),
+      col: 2,
+      rowSpan: "20%",
+      height: "40%",
+    },
+    {
+      id: 2,
+      content: renderFunction(
+        "https://gulfpalms.com/wp-content/uploads/2023/10/3-mian-img.jpg"
+      ),
+      col: 3,
+      rowSpan: "20%",
+      height: "50%",
+    },
+    {
+      id: 4,
+      content: renderFunction(
+        "https://gulfpalms.com/wp-content/uploads/2023/10/kjkjkj600by600-thumb.jpg"
+      ),
+      col: 3,
+      rowSpan: "20%",
+      height: "50%",
+    },
   ];
 
   return (
-    <div>
-      <div className="p-6">
-        <ProductsGrid items={items} maxColumns={5} />
+    <div className="">
+      <div className="h-[580px]">
+        <ProductsGrid items={items} />
       </div>
     </div>
   );
--- components/home/HomeSecondProductGrid.tsx
+++ components/home/HomeSecondProductGrid.tsx
@@ -1,5 +1,97 @@
 import React from "react";
+import ProductsGrid from "../common/ProductsGrid";
+import RenderImageAndProducts from "../common/RenderImageAndProducts";
 
 export default function HomeSecondProductGrid() {
-  return <div>HomeSecondProductGrid</div>;
+  const renderFunction = (url: any) => {
+    return (
+      <RenderImageAndProducts
+        renderType="image"
+        imageFileOrUrl={`${url}`}
+        hoverProducts={[
+          {
+            position: { x: 10, y: 15 },
+            imgUrl:
+              "https://gulfpalms.com/wp-content/uploads/2023/08/Cholorophytum-2-300x300.jpg",
+            productId: "1",
+            price: "25.99",
+            description: "Product 1",
+          },
+          {
+            position: { x: 50, y: 50 },
+            imgUrl:
+              "https://gulfpalms.com/wp-content/uploads/2023/08/Cholorophytum-2-300x300.jpg",
+            productId: "2",
+            price: "39.99",
+            description: "Product 2",
+          },
+        ]}
+      />
+    );
+  };
+
+  const items = [
+    {
+      id: 5,
+      content: renderFunction(
+        "https://gulfpalms.com/wp-content/uploads/2023/10/jjf600by800-thumb.jpg"
+      ),
+      col: 1,
+      rowSpan: "20%",
+      height: "60%",
+    },
+    {
+      id: 1,
+      content: renderFunction(
+        "https://gulfpalms.com/wp-content/uploads/2023/09/600by600-thumb5.jpg"
+      ),
+      col: 1,
+      rowSpan: "20%",
+      height: "40%",
+    },
+    {
+      id: 3,
+      content: renderFunction(
+        "https://gulfpalms.com/wp-content/uploads/2023/10/lkl600by800-thumb.jpg"
+      ),
+      col: 2,
+      rowSpan: "20%",
+      height: "40%",
+    },
+    {
+      id: 2,
+      content: renderFunction(
+        "https://gulfpalms.com/wp-content/uploads/2023/10/e600by800-thumb.jpg"
+      ),
+      col: 2,
+      rowSpan: "20%",
+      height: "60%",
+    },
+    {
+      id: 4,
+      content: renderFunction(
+        "https://gulfpalms.com/wp-content/uploads/2023/10/q600by800-thumb.jpg"
+      ),
+      col: 3,
+      rowSpan: "20%",
+      height: "60%",
+    },
+    {
+      id: 6,
+      content: renderFunction(
+        "https://gulfpalms.com/wp-content/uploads/2023/10/w600by800-thumb.jpg"
+      ),
+      col: 3,
+      rowSpan: "20%",
+      height: "60%",
+    },
+  ];
+
+  return (
+    <div className="">
+      <div className="h-[580px]">
+        <ProductsGrid items={items} />
+      </div>
+    </div>
+  );
 }
