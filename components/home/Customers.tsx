737f524b314b437106371822873c7f8fc6c153c4
development@bevycommerce.com <development@bevycommerce.com>
2025-02-27 17:04:38 +0600

    add locale for home pages

--- components/home/Customers.tsx
+++ components/home/Customers.tsx
@@ -81,13 +81,13 @@ export default function Customers() {
     <div className="container mx-auto px-4 max-w-[1458px]">
        <div className="pb-[30px] space-y-2 md:space-y-6 max-w-[800px] ">
         <p className="text-[#777777] text-xl md:text-[30px] font-light font-sans">
-          {t("customer.services")}
+          {t("customers.ourServices")}
         </p>
         <p className="text-[#242424] font-bold text-2xl md:text-[36px] font-arabic">
-          {t("customer.customerSays")}
+          {t("customers.title")}
         </p>
         <p className="text-[#777777] text-sm md:text-[16px] font-sans">
-          {t("customer.gulfPalmsExperience")}
+          {t("customers.description")}
         </p>
       </div>
       <div className="pb-8 md:pb-[50px]">
