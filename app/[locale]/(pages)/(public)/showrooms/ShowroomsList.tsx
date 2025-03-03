2fef76577cba3944ab175d3017456aa74622edaf
Sourov Basu <souravbasu123456@gmail.com>
2025-03-03 13:52:07 +0600

    new showrooms fixed

--- app/[locale]/(pages)/(public)/showrooms/ShowroomsList.tsx
+++ app/[locale]/(pages)/(public)/showrooms/ShowroomsList.tsx
@@ -88,7 +88,7 @@ export default function ShowRoomsList() {
           />
         </div>
       ))}
-      <div className="w-full max-w-[1140px] flex items-center mx-auto mb-12">
+      <div className="w-full max-w-[1140px] flex flex-col lg:flex-row items-center gap-12 lg:gap-0 mx-auto my-12">
         {showrooms.slice(3).map((showroom: ServiceProps, index: number) => (
           <div
             key={index}
@@ -111,11 +111,7 @@ export default function ShowRoomsList() {
             </h2>
             <div className="flex gap-7">
               <Link
-                href={
-                  language === "en"
-                    ? "/en/contact-us"
-                    : "/ar/contact-us</div></div>"
-                }
+                href={language === "en" ? "/en/contact-us" : "/ar/contact-us"}
                 className="bg-white border-[2px] border-primary px-5 py-2 hover:bg-primary font-semibold text-[.8125rem] text-primary duration-300 cursor-pointer"
               >
                 {language === "en" ? "CONTACT US" : "اتصل بنا"}
@@ -130,7 +126,7 @@ export default function ShowRoomsList() {
           </div>
         ))}
       </div>
-      <GetInTouch />
+      <GetInTouch language={language} />
     </div>
   );
 }
