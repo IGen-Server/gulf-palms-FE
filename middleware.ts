115cd01ab5bb37bf33331a67fc657a652e2c18fe
Md. Arafat Hossain <arafathossainarafat6@gmail.com>
2025-03-04 01:03:35 +0600

    Fix default locale issue

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,24 @@
 import { i18nRouter } from "next-i18n-router";
 import i18nConfig from "./i18nConfig";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
+// Prev middleware
+// export function middleware(request: NextRequest) {
+//   return i18nRouter(request, i18nConfig);
+// }
+
+// Updated middleware
 export function middleware(request: NextRequest) {
+  const url = request.nextUrl.clone();
+  const pathSegments = request.nextUrl.pathname.split('/');
+  const hasLocale = i18nConfig.locales.includes(pathSegments[1]);
+  const i18nCookieValue = request.cookies.get(i18nConfig.localeCookie || '')?.value;
+
+  if (!hasLocale && (!i18nCookieValue || i18nCookieValue !== i18nConfig.defaultLocale)) {
+    url.pathname = `/${i18nConfig.defaultLocale}${url.pathname}`;
+    return NextResponse.redirect(url);
+  }
+  
   return i18nRouter(request, i18nConfig);
 }
 
