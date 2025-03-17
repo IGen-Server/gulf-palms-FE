b5a30c35f08eeed010ff80d62e435e882e03b860
Md. Arafat Hossain <arafathossainarafat6@gmail.com>
2025-03-18 02:18:59 +0600

    Add demo payment

---
+++ app/[locale]/(pages)/(public)/payment/failure/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function FailurePage() {
+  return (
+    <div className='h-60 bg-red-500 mt-24 text-white text-4xl text-center'>
+      Failure Page
+    </div>
+  )
+}
---
+++ app/[locale]/(pages)/(public)/payment/success/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SuccessPage() {
+  return (
+    <div className='h-60 bg-green-500 mt-24 text-white text-4xl text-center'>
+      Success Page
+    </div>
+  )
+}
