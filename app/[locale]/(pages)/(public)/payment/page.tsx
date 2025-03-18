bd2526633f026cddf4029bfe3dbbfe4a819a2dee
Sourov Basu <souravbasu123456@gmail.com>
2025-03-19 03:55:05 +0600

    Build error fixed

--- app/[locale]/(pages)/(public)/payment/page.tsx
+++ app/[locale]/(pages)/(public)/payment/page.tsx
@@ -1,49 +1,49 @@
 'use client'
 
-import { Button } from '@/components/ui/button'
-import { PaymentRequestModel } from '@/models/payment/payment-request.model';
-import { useCart } from '@/providers/CartProvider';
-import { PaymentService } from '@/services/api/payment.service';
-import CreateAxiosInstanceWithLoader from '@/services/utility/axios-with-loader.service';
+// import { Button } from '@/components/ui/button'
+// import { PaymentRequestModel } from '@/models/payment/payment-request.model';
+// import { useCart } from '@/providers/CartProvider';
+// import { PaymentService } from '@/services/api/payment.service';
+// import CreateAxiosInstanceWithLoader from '@/services/utility/axios-with-loader.service';
 
 export default function PaymentPage() {
-  const axiosInstanceWithLoader = CreateAxiosInstanceWithLoader();
-  const axiosInstanceWithoutLoader = CreateAxiosInstanceWithLoader(false, false);
-  const { cartItems, subtotal, total, shippingCost } = useCart()
+  // const axiosInstanceWithLoader = CreateAxiosInstanceWithLoader();
+  // const axiosInstanceWithoutLoader = CreateAxiosInstanceWithLoader(false, false);
+  // const { cartItems, subtotal, total, shippingCost } = useCart()
 
 
-  function paymentTest() {
-    const paymentData: PaymentRequestModel = {
-      products: cartItems.map((product) => {
-        return ({ id: product.id as number, quantity: product.quantity })
-      }),
-      email: 'john.doe@example.com',
-      customerName: 'John Doe',
-      mobileNumber: '12345678',
-      address: '123 Street, City, Country',
-      lang: 'en',
-    };
+  // function paymentTest() {
+  //   const paymentData: PaymentRequestModel = {
+  //     products: cartItems.map((product) => {
+  //       return ({ id: product.id as number, quantity: product.quantity })
+  //     }),
+  //     email: 'john.doe@example.com',
+  //     customerName: 'John Doe',
+  //     mobileNumber: '12345678',
+  //     address: '123 Street, City, Country',
+  //     lang: 'en',
+  //   };
 
-    PaymentService.Pay(paymentData, axiosInstanceWithLoader)
-      .then(response => {
-        console.log(response.Data.PaymentURL);
-        if (response?.Data?.PaymentURL) {
-          // Redirect to MyFatoorah payment page
-          window.location.href = response.Data.PaymentURL;
-        } else {
-          console.error('Payment URL not found in response', response);
-        }
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }
+  //   PaymentService.Pay(paymentData, axiosInstanceWithLoader)
+  //     .then(response => {
+  //       console.log(response.Data.PaymentURL);
+  //       if (response?.Data?.PaymentURL) {
+  //         // Redirect to MyFatoorah payment page
+  //         window.location.href = response.Data.PaymentURL;
+  //       } else {
+  //         console.error('Payment URL not found in response', response);
+  //       }
+  //     })
+  //     .catch(error => {
+  //       console.error(error);
+  //     });
+  // }
 
   return (
     <div className='h-60 bg-gray-500 mt-24 text-white text-4xl text-center'>
       <h1>Payment Page</h1>
 
-      <Button onClick={paymentTest}>Pay</Button>
+      {/* <Button onClick={paymentTest}>Pay</Button> */}
     </div>
   )
 }
